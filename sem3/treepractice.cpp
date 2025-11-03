bb8f73d8cebe52d7eedb0de158dbaeaa6bd77f8e
irasodhi <irasodhi5@gmail.com>
2025-11-03 12:48:42 +0530

    Done

--- sem3/treepractice.cpp
+++ sem3/treepractice.cpp
@@ -86,6 +86,13 @@
 //     cout<<endl;
     
 // }
+
+
+
+
+
+
+
 // int main() {
 //  vector <int> sequence={1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
 //  Node* root=buildtree(sequence);
@@ -98,48 +105,69 @@
 //  levelordertraversal(root);
 // }
 
-#include <iostream>
-#include <vector>
-using namespace std;
+// #include <iostream>
+// #include <vector>
+// using namespace std;
 
 
-class Node{
-    public:
-    int d;
-    Node* left;
-    Node*right;
-    Node(int v){
-        d=v;
-        left=right=NULL;
-    }
-};
-Node *buildtree(vector <int>preorder){
-    static int indx=-1;
-    indx++;
-    if(preorder[indx]==-1){
-        return NULL;
-    }
-    Node * root=new Node(preorder[indx]);
-    root->left=buildtree(preorder);
-    root->right=buildtree(preorder);
-    return root;
+// class Node{
+//     public:
+//     int d;
+//     Node* left;
+//     Node*right;
+//     Node(int v){
+//         d=v;
+//         left=right=NULL;
+//     }
+// };
+// Node *buildtree(vector <int>preorder){
+//     static int indx=-1;
+//     indx++;
+//     if(preorder[indx]==-1){
+//         return NULL;
+//     }
+//     Node * root=new Node(preorder[indx]);
+//     root->left=buildtree(preorder);
+//     root->right=buildtree(preorder);
+//     return root;
 
 
-};
-int height(Node* root){
-    if(root==NULL){
-        return 0;
-    }
-    int leftht=height(root->left);
-    int rightht=height(root->right);
-    return max(leftht,rightht)+1;
-}
+// };
+// int height(Node* root){
+//     if(root==NULL){
+//         return 0;
+//     }
+//     int leftht=height(root->left);
+//     int rightht=height(root->right);
+//     return max(leftht,rightht)+1;
+// };
+// int count(Node*root){
+//     if(root==NULL){
+//         return 0;
+//     }
+//     int leftcount=count(root->left);
+//     int rightcount=count(root->right);
+//     return leftcount+rightcount+1;
+// }
+// int sumofnodes(Node* root){
+//     if(root==NULL){
+//         return 0;
+//     }
+//     int leftsum=sumofnodes(root->left);
+//     int rightsum=sumofnodes(root->right);
+//     return leftsum+rightsum+root->d;
 
-int main() {
-    vector <int> sequence={1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
-    Node* root=buildtree(sequence);
-    cout<<height(root);
-}
+// }
+
+// int main() {
+//     vector <int> sequence={1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+//     Node* root=buildtree(sequence);
+//     cout<<height(root)<<endl;
+//     cout<<count(root)<<endl;
+//     cout<<sumofnodes(root)<<endl;
+
+
+// }
 
 
 
@@ -239,3 +267,72 @@ int main() {
 //     return 0;
 // }
 
+
+
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <map>
+using namespace std;
+
+
+
+class Node{
+    public:
+    int d;
+    Node* left;
+    Node* right;
+    Node(int v){
+        d=v;
+        left=right=NULL;
+    }
+    
+};
+Node *buildtree(vector <int>&preorder ){
+    static int indx=-1;
+    indx++;
+    if(preorder[indx]==-1){
+        return 0;
+    }
+    Node* root=new Node(preorder[indx]);
+    root->left=buildtree(preorder);
+    root->right=buildtree(preorder);
+    return root;
+
+};
+void topview(Node *root){
+    queue<pair<Node*,int>>q;
+    map<int,int>m;
+    q.push({root,0});
+   
+    while(q.size()>0){
+        Node* curr=q.front().first;
+        int currhd=q.front().second;
+        q.pop();
+        if(m.find(currhd)==m.end()){
+            m[currhd]=curr->d;
+        }
+        if(curr->left!=NULL){
+            q.push({curr->left,currhd-1});
+
+        }
+        if(curr->right!=NULL){
+            q.push({curr->right,currhd+1});
+        }
+    }
+    for(auto it:m){
+        cout<<it.second;
+    }
+    cout<<endl;
+
+
+}
+
+
+
+
+int main() {
+  vector <int> sequence={1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+    Node* root=buildtree(sequence);
+    topview(root);
+}
