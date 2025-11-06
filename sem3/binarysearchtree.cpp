e7e9b9e506b4cc8be443c161f0422782018595ff
irasodhi <irasodhi5@gmail.com>
2025-11-06 14:36:13 +0530

    done

--- sem3/binarysearchtree.cpp
+++ sem3/binarysearchtree.cpp
@@ -88,7 +88,21 @@ void inorder(Node* root){
     cout<<root->data<<" ";
     inorder(root->right);
 }
+bool search(Node *root,int k){
+    if(root==NULL){
+        return false;
+    }
+    if(root->data==k){
+        return true;
+    }
+    if(root->data>k){
+        return search(root->left,k);
+    }
+    else{
+        return search(root->right,k);
+    }
 
+}
 int main() {
     vector<int> arr = {5, 3, 7, 2, 4, 6, 8};
     Node* root = buildbst(arr);
