7159663fe469967c385ed568dca45a931172c654
irasodhi <irasodhi5@gmail.com>
2025-11-06 14:14:00 +0530

    done

---
+++ sem3/binarysearchtree.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node *left;
+    Node* right;
+    Node(int v){
+        data=v;
+        left=right=NULL;
+    }
+};
+Node *insert(Node* root,int val){
+    if(root==NULL){
+        return new Node(val);
+    }
+    if(val<root->data){
+        root->left=insert(root->left,val);
+    }
+    else{
+        root->right=insert(root->right,val);
+    }
+    return root;
+}
+Node *buildbst(vector <int>arr){
+  Node *root=NULL;
+  for(int val:arr){
+    root=insert(root,val);
+  }
+  return root;
+}
+void inorder(Node*root){
+    if(!root)return;
+    inorder(root->left);
+    cout<<root->data;
+    inorder(root->right);
+}
+
+int main() {
+      vector<int> arr = {5, 3, 7, 2, 4, 6, 8};
+    Node* root = buildbst(arr);
+
+    cout << "Inorder traversal of BST: ";
+    inorder(root);
+    cout << endl;
+}
