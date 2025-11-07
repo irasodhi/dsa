9ec56a077175262c326797ca8d1e6900fdc73b61
irasodhi <irasodhi5@gmail.com>
2025-11-07 18:38:13 +0530

    done

--- sem3/practice.cpp
+++ sem3/practice.cpp
@@ -57,54 +57,54 @@ int main() {
   travers(head);
   
 }
-#include <iostream>
+// #include <iostream>
 
-struct Node {
-    int data;
-    Node* next;
-    Node(int val) : data(val), next(nullptr) {}
-};
+// struct Node {
+//     int data;
+//     Node* next;
+//     Node(int val) : data(val), next(nullptr) {}
+// };
 
-Node* insertSorted(Node* head, int value) {
-    Node* newNode = new Node(value);
-    if (head == nullptr || head->data >= value) {
-        newNode->next = head;
-        return newNode; 
-    }
+// Node* insertSorted(Node* head, int value) {
+//     Node* newNode = new Node(value);
+//     if (head == nullptr || head->data >= value) {
+//         newNode->next = head;
+//         return newNode; 
+//     }
     
-    Node* current = head;
-    while (current->next != nullptr && current->next->data < value) {
-        current = current->next;
-    }
-    newNode->next = current->next;
-    current->next = newNode;
-    return head;
-}
+//     Node* current = head;
+//     while (current->next != nullptr && current->next->data < value) {
+//         current = current->next;
+//     }
+//     newNode->next = current->next;
+//     current->next = newNode;
+//     return head;
+// }
 
-void traverseList(Node* head) {
-    Node* current = head;
-    while (current != nullptr) {
-        std::cout << current->data << " ";
-        current = current->next;
-    }
-    std::cout << std::endl;
-}
+// void traverseList(Node* head) {
+//     Node* current = head;
+//     while (current != nullptr) {
+//         std::cout << current->data << " ";
+//         current = current->next;
+//     }
+//     std::cout << std::endl;
+// }
 
-int main() {
-    Node* head = new Node(10);
-    head->next = new Node(20);
-    head->next->next = new Node(30);
+// int main() {
+//     Node* head = new Node(10);
+//     head->next = new Node(20);
+//     head->next->next = new Node(30);
     
-    head = insertSorted(head, 15); // Insert 15
-    std::cout << "Sorted list: ";
-    traverseList(head);
+//     head = insertSorted(head, 15); // Insert 15
+//     std::cout << "Sorted list: ";
+//     traverseList(head);
     
-    // Clean up
-    while (head != nullptr) {
-        Node* temp = head;
-        head = head->next;
-        delete temp;
-    }
-    return 0;
-}
+//     // Clean up
+//     while (head != nullptr) {
+//         Node* temp = head;
+//         head = head->next;
+//         delete temp;
+//     }
+//     return 0;
+// }
 
