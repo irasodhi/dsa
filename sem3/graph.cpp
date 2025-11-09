c3d2afc41125944d81dfd74f8bcc96a01bb08874
irasodhi <irasodhi5@gmail.com>
2025-11-10 00:56:32 +0530

    done

--- sem3/graph.cpp
+++ sem3/graph.cpp
@@ -150,66 +150,97 @@
 
 
 
-#include <iostream>
-#include<queue>
-#include<vector>
-#include<list>
+// #include <iostream>
+// #include<queue>
+// #include<vector>
+// #include<list>
 
-using namespace std;
-class Graph{
-    int V;
-    list<int>*l;
-    public:
-    Graph(int V){
-        this->V=V;
-        l=new list<int>[V];
-    }
-    void addedge(int u,int v){
-        l[u].push_back(v);
-        l[v].push_back(u);
-    }
-    void bfs(){
-        queue<int>q;
-        vector<bool>vis(V,false);
-        q.push(0);
-        vis[0]=true;
-        while(q.size()>0){
-            int u=q.front();
-            q.pop();
-            cout<<u<<" ";
-            for(int v:l[u]){
-                if(!vis[v]){
-                    vis[v]=true;
-                    q.push(v);
-                }
-            }
-        }
+// using namespace std;
+// class Graph{
+//     int V;
+//     list<int>*l;
+//     public:
+//     Graph(int V){
+//         this->V=V;
+//         l=new list<int>[V];
+//     }
+//     void addedge(int u,int v){
+//         l[u].push_back(v);
+//         l[v].push_back(u);
+//     }
+//     void bfs(){
+//         queue<int>q;
+//         vector<bool>vis(V,false);
+//         q.push(0);
+//         vis[0]=true;
+//         while(q.size()>0){
+//             int u=q.front();
+//             q.pop();
+//             cout<<u<<" ";
+//             for(int v:l[u]){
+//                 if(!vis[v]){
+//                     vis[v]=true;
+//                     q.push(v);
+//                 }
+//             }
+//         }
 
-        cout<<endl;
-    }
-    void dfshelper(int u,vector<bool>&vis){
-      cout<<u<<" ";
-      vis[u]=true;
-      for(int v:l[u]){
-        if(!vis[v]){
-            dfshelper(v,vis);
-        }
-      }
-    }
-    void dfs(){
-        int src=0;
-        vector<bool>vis(V,false);
-        dfshelper(src,vis);
-    }
-};
+//         cout<<endl;
+//     }
+//     void dfshelper(int u,vector<bool>&vis){
+//       cout<<u<<" ";
+//       vis[u]=true;
+//       for(int v:l[u]){
+//         if(!vis[v]){
+//             dfshelper(v,vis);
+//         }
+//       }
+//     }
+//     void dfs(){
+//         int src=0;
+//         vector<bool>vis(V,false);
+//         dfshelper(src,vis);
+//     }
+//     bool iscycle(int src,int par,vector<bool>&vis){
+//         vis[src]=true;
+//         list<int>neighbors=l[src];
+//         for(int v:neighbors){
+//             if(!vis[v]){
+//                 if (iscycle(v,src,vis)){
+//                     return true;
+//                 }
+//             }
+//             else if(v!=par){
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+//     bool isCycle(){
+//         vector<bool>vis(V,false);
+//         for(int i=0;i<V;i++){
+//             if(!vis[i]){
+//                 if(iscycle(i,-1,vis)){
+//                     return true;
+//                 }
+//             }
+//         }
+//         return false;
+//     }
+// };
 
-int main() {
-   Graph g(5);
-   g.addedge(0,1);
-   g.addedge(1,2);
-   g.addedge(1,3);
-   g.addedge(2,4);
-   g.bfs();
-   g.dfs();
-   return 0;
-}
+// int main() {
+//    Graph g(5);
+//    g.addedge(0,1);
+//    g.addedge(1,2);
+//    g.addedge(1,3);
+//    g.addedge(2,4);
+//    g.bfs();
+//    g.dfs();
+//    if(g.isCycle())
+//    cout<<"cycledetected";
+//    else{
+//     cout<<"cyclenotfound";
+//    }
+//    return 0;
+// }
