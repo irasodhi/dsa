c7326d93ab25a57bcf82776bd845698161f014a9
irasodhi <irasodhi5@gmail.com>
2025-11-09 16:02:34 +0530

    done

--- sem3/heap.cpp
+++ sem3/heap.cpp
@@ -24,11 +24,50 @@ class heap{
             }
         }
      }
+     void deletefr(){
+        if(size==0){
+            cout<<"Nothing to delete";
+            return;
+        }
+        arr[1]=arr[size];
+        size--;
+        int i=1;
+        while(i<size){
+            int left=2*i;
+            int right=2*i+1;
+            if(left<=size &&arr[i]<arr[left]){
+                swap(arr[i],arr[left]);
+                i=left;
+            }
+            else if(right<=size&& arr[i]<arr[right]){
+                swap(arr[i],arr[right]);
+                i=right;
+            }
+            else{
+                return;
+            }
+        }
+     }
      void print(){
         for(int i=1;i<=size;i++){
             cout<<arr[i]<<" ";
         }
         cout<<endl;
+     }
+     void heapify(int arr[],int n,int i){
+        int largest=i;
+        int left=2*i;
+        int right=2*i+1;
+        if(left<=n&&arr[largest]<arr[left]){
+            largest=left;
+        }
+        else if(right<=n&&arr[largest]<arr[right]){
+            largest=right;
+        }
+        if(largest!=i){
+            swap(arr[largest],arr[i]);
+            heapify(arr,n,largest);
+        }
      }
 };
 
@@ -39,5 +78,15 @@ int main() {
    h.insert(53);
    h.insert(52);
    h.insert(54);
+   h.deletefr();
    h.print();
+   int arr[6]={-1,54,53,55,52,50};
+   int n=5;
+   for(int i=n/2;i>0;i--){
+    h.heapify(arr,n,i);
+   }
+   for(int i=1;i<=n;i++){
+    cout<<arr[i]<<" ";
+   }
+   return 0;
 }
