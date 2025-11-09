3e9fd1b61be1b42ee73f4ebeee3bc82b63ac407c
irasodhi <irasodhi5@gmail.com>
2025-11-09 18:29:06 +0530

    done

--- sem3/graph.cpp
+++ sem3/graph.cpp
@@ -146,3 +146,56 @@
 //         int rh=height(root->right);
 //         return max(lh,rh)+1;
 //     }
+
+
+
+
+#include <iostream>
+#include<queue>
+#include<vector>
+#include<list>
+
+using namespace std;
+class Graph{
+    int V;
+    list<int>*l;
+    public:
+    Graph(int V){
+        this->V=V;
+        l=new list<int>[V];
+    }
+    void addedge(int u,int v){
+        l[u].push_back(v);
+        l[v].push_back(u);
+    }
+    void bfs(){
+        queue<int>q;
+        vector<bool>vis(V,false);
+        q.push(0);
+        vis[0]=true;
+        while(q.size()>0){
+            int u=q.front();
+            q.pop();
+            cout<<u<<" ";
+            for(int v:l[u]){
+                if(!vis[v]){
+                    vis[v]=true;
+                    q.push(v);
+                }
+            }
+        }
+
+        cout<<endl;
+    }
+};
+
+int main() {
+   Graph g(5);
+   g.addedge(0,1);
+   g.addedge(1,2);
+   g.addedge(1,3);
+   g.addedge(2,3);
+   g.addedge(2,4);
+   g.bfs();
+   return 0;
+}
