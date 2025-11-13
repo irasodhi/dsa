2c0248f93ebf3816c62084c05e269859cece1054
irasodhi <irasodhi5@gmail.com>
2025-11-13 18:56:21 +0530

    done

--- sem3/hash.cpp
+++ sem3/hash.cpp
@@ -43,3 +43,21 @@ public:
     }
     
 };
+
+class Solution {
+public:
+    int maxOperations(string s) {
+        long long ans=0;
+        long long ones=0;
+        int n=s.size();
+        for(int i=0;i<n;i++){
+            if(s[i]=='1'){
+                ones++;
+            }
+            else{
+                if(i+1==n||s[i+1]=='1')ans+=ones;
+            }
+        }
+        return (int)ans;
+    }
+};
